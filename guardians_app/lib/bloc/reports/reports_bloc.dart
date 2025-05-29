6d065263466e668899c1a00e35676a0ef886a1a8
giwrgosisf <164655527+giwrgosisf@users.noreply.github.com>
2025-05-29 09:39:24 +0300

    last fix

--- guardians_app/lib/bloc/reports/reports_bloc.dart
+++ guardians_app/lib/bloc/reports/reports_bloc.dart
@@ -1,3 +1,110 @@
+// import 'dart:async';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'reports_event.dart';
+// import 'reports_state.dart';
+// import '../../data/models/kid_profile.dart';
+// import '../../data/repositories/reports_repository.dart';
+//
+// class ReportsBloc extends Bloc<ReportsEvent, ReportsState> {
+//   final ReportsRepository _reportsRepository;
+//   StreamSubscription<List<KidProfile>>? _reportsSubscription;
+//
+//   ReportsBloc(this._reportsRepository) : super(ReportsInitial()) {
+//     on<LoadReports>(_onLoadReports);
+//     on<MarkMessageAsReviewed>(_onMarkMessageAsReviewed);
+//     on<RefreshReports>(_onRefreshReports);
+//
+//     print('DEBUG: ReportsBloc initialized');
+//   }
+//
+//   Future<void> _onLoadReports(
+//     LoadReports event,
+//     Emitter<ReportsState> emit,
+//   ) async {
+//     print('DEBUG: _onLoadReports called with kidIds: ${event.kidIds}');
+//     if (isClosed) {
+//       print('DEBUG: Bloc is already closed, skipping load');
+//       return;
+//     }
+//     emit(ReportsLoading());
+//     print('DEBUG: Emitted ReportsLoading');
+//     try {
+//       await _reportsSubscription?.cancel();
+//       print('DEBUG: Cancelled previous subscription');
+//
+//       _reportsSubscription = _reportsRepository
+//           .watchFlaggedReports(event.kidIds)
+//           .listen(
+//             (kidsWithFlags) {
+//               print('DEBUG: Stream received ${kidsWithFlags.length} kids');
+//               for (var kid in kidsWithFlags) {
+//                 print('DEBUG: Stream - Kid ${kid.firstName} has ${kid.flaggedMessages.length} flagged messages');
+//               }
+//
+//               if (!emit.isDone && !isClosed) {
+//                 print('DEBUG: Emitting ReportsLoaded with ${kidsWithFlags.length} kids');
+//                 emit(ReportsLoaded(kidsWithFlags: kidsWithFlags));
+//                 print('DEBUG: Successfully emitted ReportsLoaded');
+//               } else {
+//                 print('DEBUG: Emitter is done or bloc is closed, skipping emit');
+//               }
+//             },
+//             onError: (error) {
+//               print('DEBUG: Stream error: $error');
+//               if (!emit.isDone && !isClosed) {
+//                 emit(ReportsError('Failed to load reports: $error'));
+//               }
+//             },
+//           );
+//       print('DEBUG: Stream subscription established');
+//     } catch (e) {
+//       print('DEBUG: Exception in _onLoadReports: $e');
+//       if (!emit.isDone && !isClosed) {
+//         emit(ReportsError('Failed to load reports: $e'));
+//       }
+//     }
+//   }
+//
+//   Future<void> _onMarkMessageAsReviewed(
+//     MarkMessageAsReviewed event,
+//     Emitter<ReportsState> emit,
+//   ) async {
+//     // TODO: Implement marking message as reviewed in Firestore
+//     // await _reportsRepository.markMessageAsReviewed(
+//     //   event.kidId,
+//     //   event.messageId,
+//     //   event.isOffensive
+//     // );
+//   }
+//
+//   Future<void> _onRefreshReports(
+//     RefreshReports event,
+//     Emitter<ReportsState> emit,
+//   ) async {
+//     print('DEBUG: _onRefreshReports called');
+//     if (state is ReportsLoaded && !isClosed ) {
+//       final currentState = state as ReportsLoaded;
+//       emit(currentState.copyWith(isRefreshing: true));
+//
+//       await Future.delayed(const Duration(milliseconds: 250));
+//
+//       if (!emit.isDone && !isClosed && state is ReportsLoaded) {
+//         final newState = state as ReportsLoaded;
+//         emit(newState.copyWith(isRefreshing: false));
+//       }
+//     }
+//   }
+//
+//   @override
+//   Future<void> close() {
+//     print('DEBUG: ReportsBloc closing');
+//     _reportsSubscription?.cancel();
+//     return super.close();
+//   }
+// }
+
+
+
 import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'reports_event.dart';
@@ -7,7 +114,6 @@ import '../../data/repositories/reports_repository.dart';
 
 class ReportsBloc extends Bloc<ReportsEvent, ReportsState> {
   final ReportsRepository _reportsRepository;
-  StreamSubscription<List<KidProfile>>? _reportsSubscription;
 
   ReportsBloc(this._reportsRepository) : super(ReportsInitial()) {
     on<LoadReports>(_onLoadReports);
@@ -18,57 +124,45 @@ class ReportsBloc extends Bloc<ReportsEvent, ReportsState> {
   }
 
   Future<void> _onLoadReports(
-    LoadReports event,
-    Emitter<ReportsState> emit,
-  ) async {
+      LoadReports event,
+      Emitter<ReportsState> emit,
+      ) async {
     print('DEBUG: _onLoadReports called with kidIds: ${event.kidIds}');
     if (isClosed) {
       print('DEBUG: Bloc is already closed, skipping load');
       return;
     }
+
     emit(ReportsLoading());
     print('DEBUG: Emitted ReportsLoading');
+
     try {
-      await _reportsSubscription?.cancel();
-      print('DEBUG: Cancelled previous subscription');
-
-      _reportsSubscription = _reportsRepository
-          .watchFlaggedReports(event.kidIds)
-          .listen(
-            (kidsWithFlags) {
-              print('DEBUG: Stream received ${kidsWithFlags.length} kids');
-              for (var kid in kidsWithFlags) {
-                print('DEBUG: Stream - Kid ${kid.firstName} has ${kid.flaggedMessages.length} flagged messages');
-              }
-
-              if (!emit.isDone && !isClosed) {
-                print('DEBUG: Emitting ReportsLoaded with ${kidsWithFlags.length} kids');
-                emit(ReportsLoaded(kidsWithFlags: kidsWithFlags));
-                print('DEBUG: Successfully emitted ReportsLoaded');
-              } else {
-                print('DEBUG: Emitter is done or bloc is closed, skipping emit');
-              }
-            },
-            onError: (error) {
-              print('DEBUG: Stream error: $error');
-              if (!emit.isDone && !isClosed) {
-                emit(ReportsError('Failed to load reports: $error'));
-              }
-            },
-          );
-      print('DEBUG: Stream subscription established');
+      print('DEBUG: Starting emit.forEach');
+      await emit.forEach<List<KidProfile>>(
+        _reportsRepository.watchFlaggedReports(event.kidIds),
+        onData: (kidsWithFlags) {
+          print('DEBUG: emit.forEach received ${kidsWithFlags.length} kids');
+          for (var kid in kidsWithFlags) {
+            print('DEBUG: emit.forEach - Kid ${kid.firstName} has ${kid.flaggedMessages.length} flagged messages');
+          }
+          return ReportsLoaded(kidsWithFlags: kidsWithFlags);
+        },
+        onError: (error, stackTrace) {
+          print('DEBUG: emit.forEach error: $error');
+          return ReportsError('Failed to load reports: $error');
+        },
+      );
+      print('DEBUG: emit.forEach completed');
     } catch (e) {
       print('DEBUG: Exception in _onLoadReports: $e');
-      if (!emit.isDone && !isClosed) {
-        emit(ReportsError('Failed to load reports: $e'));
-      }
+      emit(ReportsError('Failed to load reports: $e'));
     }
   }
 
   Future<void> _onMarkMessageAsReviewed(
-    MarkMessageAsReviewed event,
-    Emitter<ReportsState> emit,
-  ) async {
+      MarkMessageAsReviewed event,
+      Emitter<ReportsState> emit,
+      ) async {
     // TODO: Implement marking message as reviewed in Firestore
     // await _reportsRepository.markMessageAsReviewed(
     //   event.kidId,
@@ -78,27 +172,20 @@ class ReportsBloc extends Bloc<ReportsEvent, ReportsState> {
   }
 
   Future<void> _onRefreshReports(
-    RefreshReports event,
-    Emitter<ReportsState> emit,
-  ) async {
+      RefreshReports event,
+      Emitter<ReportsState> emit,
+      ) async {
     print('DEBUG: _onRefreshReports called');
-    if (state is ReportsLoaded && !isClosed ) {
+    if (state is ReportsLoaded && !isClosed) {
       final currentState = state as ReportsLoaded;
       emit(currentState.copyWith(isRefreshing: true));
 
       await Future.delayed(const Duration(milliseconds: 250));
 
-      if (!emit.isDone && !isClosed && state is ReportsLoaded) {
+      if (!isClosed && state is ReportsLoaded) {
         final newState = state as ReportsLoaded;
         emit(newState.copyWith(isRefreshing: false));
       }
     }
-  }
-
-  @override
-  Future<void> close() {
-    print('DEBUG: ReportsBloc closing');
-    _reportsSubscription?.cancel();
-    return super.close();
   }
 }
